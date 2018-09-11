f04fe5c604658e47d3d88088a96e39adc6238b87
Alvin Voo <xoalvinox@gmail.com>
2018-09-11 18:59:31 +0800

    refactor: code refactored with eslint

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,19 @@
-import React, {Component} from 'react';//work with components
+import React, { Component } from 'react';// work with components
+import { connect } from 'react-redux';
 import Search from './search';
 
-import {connect} from 'react-redux';
 
-class Index extends Component{
-
+class Index extends Component {
   static getInitialProps({ reduxStore, req }) {
-    //this is the initial props function in server
+    // this is the initial props function in server
     const isServer = !!req;
-    console.log('server alive? ',isServer);
+    console.log('server alive? ', isServer);
     return {};
   }
 
-  render(){
-    return <Search/>;
+  render() {
+    return <Search />;
   }
-
 }
 
 export default connect()(Index);
