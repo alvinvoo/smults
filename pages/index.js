7dfbee2d354eebee319e5a63dc97fe79136e82c3
Alvin Voo <xoalvinox@gmail.com>
2018-09-16 13:42:28 +0800

    fix: exporting the component for testing plus a bit of refactoring

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';// work with components
 import { connect } from 'react-redux';
-import Search from './search';
+import ConnectedSearch from './search';
 
 
-class Index extends Component {
+export class Index extends Component {
   static getInitialProps({ reduxStore, req }) {
     // this is the initial props function in server
     const isServer = !!req;
-    console.log('server alive? ', isServer);
-    return {};
+    return { isServer };
   }
 
   render() {
-    return <Search />;
+    return <ConnectedSearch />;
   }
 }
 
