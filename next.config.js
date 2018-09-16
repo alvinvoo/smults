fad7768ae38b04025924289b35c582f8667f869a
Alvin Voo <xoalvinox@gmail.com>
2018-09-16 13:40:37 +0800

    refactor: code refactor due to ESLint

--- next.config.js
+++ next.config.js
@@ -1,18 +1,19 @@
 // next.config.js
-const withCSS = require('@zeit/next-css')
+const withCSS = require('@zeit/next-css');
+
 module.exports = withCSS({
-  webpack (config, options) {
-    if(!options.isServer){
+  webpack(config, options) {
+    if (!options.isServer) {
       config.module.rules.push({
         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
         use: {
           loader: 'url-loader',
           options: {
-            limit: 100000
-          }
-        }
-      })
+            limit: 100000,
+          },
+        },
+      });
     }
-    return config
-  }
-})
+    return config;
+  },
+});
