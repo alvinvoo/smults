92016e6be69a324bfdb25d94ddd240f6c97a2aee
Alvin Voo <xoalvinox@gmail.com>
2018-09-18 15:07:59 +0800

    fix: move all stylesheet links to _document

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Container } from 'semantic-ui-react';
-// import 'semantic-ui-css/semantic.min.css';
-import '../css/style.css';
-import Head from 'next/head';
 import Header from './Header';
 import Footer from './Footer';
 
@@ -11,9 +8,6 @@ export default function Layout(props) {
   const { item, children } = props;
   return (
     <Container style={{ padding: '2em 0em' }}>
-      <Head>
-        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css" />
-      </Head>
       <Header item={item} />
       {children}
       <Footer />
