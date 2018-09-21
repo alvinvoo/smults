5666d2eb782271affa389d98e91ad78ae016fd5e
Alvin Voo <xoalvinox@gmail.com>
2018-09-21 11:24:09 +0800

    chore: Added some FAQ on the new Author's filter function

--- pages/about.js
+++ pages/about.js
@@ -30,6 +30,9 @@ export default () => (
       </p>
       <h3>Can I add custom tags in the search bar?</h3>
       <p>Sure! Just start typing and press &apos;Enter&apos; to include the tag(s) when you are done!</p>
+      <h3>Can I search by &lsquo;Author&rsquo;?</h3>
+      <p>Of course! Just switch the &lsquo;Filter tags by..&rsquo; filter to &lsquo;Author&rsquo;, an input box will appear to the right where you can start typing and look for your favorite author! (Much like busy.org&apos;s search bar)</p>
+      <p>You can also further filter their posts down by searching with tags!</p>
       <h3>Why &apos;15&apos; trending tags to start with? Why not &apos;18&apos; or &apos;23&apos;?</h3>
       <p>Well.. That&apos;s just my personal preference ;). You can add more custom search tags by youself!</p>
       <h3>What is this &apos;Mark first tag as category&apos; checkbox thing?</h3>
@@ -37,6 +40,7 @@ export default () => (
       <h3>I spotted a nasty error/bug.. Where can I report it?</h3>
       <p>
         Head over to this project&apos;s
+        {' '}
         <a href="https://github.com/Alvin-Voo/smults" target="_blank" rel="noopener noreferrer">github page</a>
         {' '}
         and log an issue.
@@ -44,11 +48,13 @@ export default () => (
       <h3>I have a great idea for your application! I want to contribute!</h3>
       <p>
         Great! Let me know at
+        {' '}
         <a href="https://steemit.com/@alvinvoo" target="_blank" rel="noopener noreferrer">@alvinvoo</a>
-        , comment on my post which announces this project, or you can visit this project&apos;s
+        , comment on my post which announces this project, hit me up in discord (alvinvoo#3009) or you can visit this project&apos;s
+        {' '}
         <a href="https://github.com/Alvin-Voo/smults" target="_blank" rel="noopener noreferrer">github page</a>
         {' '}
-        and log an issue but with the title starting with &apos;[FEATURE]&apos;
+        and log a &lsquo;Feature request&rsquo; under the issue page.
       </p>
       <h3>Is this free?</h3>
       <p>Of course! I will let you know when it&apos;s not. ;)</p>
