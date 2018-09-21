d2533e4a4b730b2e5f887a1eae9e6dfb01ad7a36
Alvin Voo <xoalvinox@gmail.com>
2018-09-21 11:45:25 +0800

    test+improvement: Additional test cases for checking no empty values and ensure property key value and test is in place

--- tests/reducers/tags.reducer.test.js
+++ tests/reducers/tags.reducer.test.js
@@ -12,14 +12,31 @@ test('should set default state', () => {
   expect(state).toBe(initialState);
 });
 
-test(`should return a state with has ${MAX_TAGS} minus 1 (empty) tag`, async () => {
-  const store = mockStore({});
-  await store.dispatch(fetchTrendingTags());
-  const action = store.getActions()[0];
-  const state = tagsReducer(initialState, action);
-  const { trending_tags_options } = state;
-  expect(trending_tags_options instanceof Array).toBe(true);
-  expect(trending_tags_options).toHaveLength(MAX_TAGS - 1);
-  expect(trending_tags_options).toEqual(expect.arrayContaining(initialState.trending_tags_options));
-  expect(trending_tags_options).not.toEqual(expect.arrayContaining([{ key: '', value: '', text: '' }]));
+describe('Testing async functions related to tags reducers', () => {
+  let action;
+  let trending_tags_options;
+
+  beforeAll(async () => {
+    const store = mockStore({});
+    await store.dispatch(fetchTrendingTags());
+    [action] = store.getActions();
+    const state = tagsReducer(initialState, action);
+    ({ trending_tags_options } = state);
+  });
+
+  test(`should return a state with has ${MAX_TAGS} minus 1 (empty) tag`, async () => {
+    expect(trending_tags_options instanceof Array).toBe(true);
+    expect(trending_tags_options).toHaveLength(MAX_TAGS - 1);
+  });
+
+  test('should return a state which contain no empty values', () => {
+    expect(trending_tags_options).not.toEqual(expect.arrayContaining([{ value: '', text: '' }]));
+  });
+
+  test('should have property key \'value\' and \'text\' in the returned state', () => {
+    trending_tags_options.forEach((tag) => {
+      expect(tag).toHaveProperty('value');
+      expect(tag).toHaveProperty('text');
+    });
+  });
 });
